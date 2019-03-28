14ebc027e40c8750e8fa3aaf58bacd96c9c71936
BIRD-MAC <chalermporn@aspiron.co.th>
2019-03-29 01:26:05 +0700

    :sparkles: Add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
     }
 };
